564f68c908e5dcb37243ae29ef5a0e60c1c7f149
ilernah <iler.sfrdfk@gmail.com>
2017-01-30 17:44:11 +0300

    new

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : MonoBehaviour {
     public static GameManager instance = null;
 
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector]
+    public bool playersTurn = true;
 
     private int level = 3;
 
@@ -28,6 +31,11 @@ public class GameManager : MonoBehaviour {
 
     void InitGame() {
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        enabled = false;
     }
 
 	// Update is called once per frame
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    public Sprite dmgSprite;
+    public int hp = 4;
+
+    private SpriteRenderer spriteRender;
+
+	// Use this for initialization
+	void Awake () {
+        spriteRender = GetComponent<SpriteRenderer>();
+	}
+
+    public void DamageWall(int loss) {
+        spriteRender.sprite = dmgSprite;
+        hp -= loss;
+        if (hp <= 0) {
+            gameObject.SetActive(false);
+        }
+
+    }
+	
+}
